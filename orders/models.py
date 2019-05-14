d99cd32ba973802b4e237baffb5a321f2babe079
Marcin Fudala <nerko937@mail.com>
2019-05-14 15:43:28 +0200

    updated orders models

--- orders/models.py
+++ orders/models.py
@@ -2,27 +2,59 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+ORDER_TYPE = (
+	('pieces', 'surowe płytki'),
+	('chain', 'łańcuch'),
+	('welded construction', 'konstrukcja spawana')
+)
+
+
 class Order(models.Model):
 
-	name = models.CharField(max_length=255)
+	name = models.CharField(verbose_name='nazwa', max_length=255)
+	order_type = models.CharField(
+		verbose_name='typ zamówienia',
+		max_length=32,
+		choices=ORDER_TYPE)
+	barcode = models.PositiveSmallIntegerField(verbose_name='kod kreskowy')
 	creation_date = models.DateField(
+		verbose_name='data utworzenia',
 		auto_now_add=True
 	)
-	realisation_limit_date = models.DateField()
+	realisation_limit_date = models.DateField(verbose_name='termin realizacji')
 	added_by = models.ForeignKey(
 		User,
+		verbose_name='dodano przez',
 		on_delete=models.SET_NULL,
 		null=True
 	)
 
+	def __str__(self):
+		return self.name
+
+	class Meta:
+		verbose_name = 'Zamówienie'
+		verbose_name_plural = 'Zamówienia'
+
 
 class Progress(models.Model):
 
-	order = models.ForeignKey(
+	order = models.OneToOneField(
 		Order,
-		on_delete=models.CASCADE
+		verbose_name='zamówienie',
+		related_name='progress',
+		on_delete=models.CASCADE,
+		primary_key=True
 	)
-	pieces_made = models.BooleanField(default=False)
-	heat_treatment = models.NullBooleanField()
-	assembly = models.NullBooleanField()
-	finish_status = models.BooleanField(default=False)
+	pieces_made = models.BooleanField(verbose_name='zrobienie płytek', default=False)
+	heat_treatment = models.NullBooleanField(verbose_name='hartowanie')
+	assembly = models.NullBooleanField(verbose_name='montaż')
+	weld = models.NullBooleanField(verbose_name='spawanie')
+	finish_status = models.BooleanField(verbose_name='status ukończenia', default=False)
+
+	def __str__(self):
+		return f'{self.order.name}: {self.finish_status}'
+
+	class Meta:
+		verbose_name = 'Postęp'
+		verbose_name_plural = 'Postępy'
