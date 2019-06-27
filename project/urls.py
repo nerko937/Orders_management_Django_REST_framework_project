6c81f16fe067ba9ccdf7b76496127a63ec3edb1d
Marcin Fudala <nerko937@gmail.com>
2019-06-27 15:33:58 +0200

    added sign up view and template

--- project/urls.py
+++ project/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from orders.views import RegularOrderListView
-
+from users.views import SignUpView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('', RegularOrderListView.as_view()),
+    path('', RegularOrderListView.as_view(), name='main'),
+    path('sign_up/', SignUpView.as_view(), name='sign_up'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,18 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.views import View
 from rest_framework import generics
 
 from .serializers import UserSerializer
 
+
 class UserListView(generics.ListCreateAPIView):
+
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class SignUpView(View):
+
+    def get(self, request):
+        return render(request, 'sign_up.html')
