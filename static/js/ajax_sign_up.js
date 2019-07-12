2364f1f03450600b13d139892cee913329649a40
Marcin Fudala <nerko937@gmail.com>
2019-07-12 12:32:26 +0200

    Created sign up through AJAX into rest API

---
+++ static/js/ajax_sign_up.js
@@ -0,0 +1,41 @@
+$(function () {
+
+    var registerForm = $( "#sign-up" );
+    $.post( '/api/auth/registration/', registerForm.serialize() );
+    registerForm.on('submit', function ( event ) {
+
+        event.preventDefault();
+
+        $.post( '/api/auth/registration/', $( this ).serialize() )
+            .done(function() {
+                alert('rejsestracja jest')
+                var data = {
+                    "first_name" : $('#register-name').val(),
+                    "last_name" : $('#register-surname').val()
+                }
+                $.ajax({
+                    url: '/api/auth/user/',
+                    type: "PATCH",
+                    data: JSON.stringify(data),
+                    beforeSend: function(xhr) {
+                        xhr.setRequestHeader('X-CSRFToken', Cookies.get('csrftoken'))
+                    },
+                    contentType: "application/json",
+                })
+                    .fail(function(xhr,status,err) {
+                        alert('Error during patching user data')
+                        console.log(xhr)
+                        console.log(status)
+                        console.log(err)
+            });
+            })
+            .fail(function(xhr,status,err) {
+                alert('Registration error')
+                console.log(xhr)
+                console.log(status)
+                console.log(err)
+            });
+
+    });
+
+});
