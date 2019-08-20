ed6b4cfa5fac67ab965307f4f1128c2c42e9bbe5
Marcin Fudala <nerko937@gmail.com>
2019-08-20 09:26:27 +0200

    minor fix for cors urls

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -58,9 +58,11 @@ MIDDLEWARE = [
 
 CORS_ORIGIN_ALLOW_ALL = False
 
-CORS_ORIGIN_WHITELIST = (
-    'http//:localhost:8000',
-)
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+    'http://localhost:8000',
+    'http://localhost:8080',
+]
 
 ROOT_URLCONF = 'project.urls'
 
